fbc60710493d2206f9e57e8addcda9f3c00bd86b
Marco Tessitore <marcotexcode@gmail.com>
2021-09-15 14:37:50 +0200

    aggiungere relazione uno molti

--- app/Post.php
+++ app/Post.php
@@ -11,8 +11,15 @@ class Post extends Model
 
         'title',
         'slug',
-        'content'
+        'content',
+        'category_id'
 
     ];
-    
+
+    public function category() {
+
+        return $this->belongsTo('App\category');
+
+    }
+
 }
