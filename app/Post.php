2343d52464662f0b8fcc978e1385f0e47412ffc6
Marco Tessitore <marcotexcode@gmail.com>
2021-09-15 13:35:25 +0200

    creare tabella categories

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
     protected $fillable = [
 
         'title',
@@ -13,4 +14,5 @@ class Post extends Model
         'content'
 
     ];
+    
 }
